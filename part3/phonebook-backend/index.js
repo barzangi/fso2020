9bb411a47f5d1bf374a2bd8ebcd27eb9cd92a6c4
Layth Barzangi <thebarzangi@gmail.com>
2020-08-21 18:20:59 +0400

    Solved: Exercises 3.1-3.6 (Phonebook app backend) Get all entries, get single entry, delete entry, add entry. Includes basic error handling.

---
+++ part3/phonebook-backend/index.js
@@ -0,0 +1,93 @@
+const http = require('http');
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+let persons = [
+  {
+    "name": "Arto Hellas",
+    "number": "040-123456",
+    "id": 1
+  },
+  {
+    "name": "Ada Lovelace",
+    "number": "39-44-5323523",
+    "id": 2
+  },
+  {
+    "name": "Dan Abramov",
+    "number": "044-3343443",
+    "id": 3
+  },
+  {
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423122",
+    "id": 4
+  }
+];
+
+// all persons
+app.get('/api/persons', (req, res) => {
+  res.json(persons);
+});
+
+// special info page
+app.get('/info', (req, res) => {
+  res.send(
+    `
+    <p>Phonebook has info for ${persons.length} people</p>
+    <p>${new Date()}</p>
+    `
+  );
+});
+
+// return single person info
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find(person => person.id === id);
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).end();
+  }
+});
+
+// delete person
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+  persons = persons.filter(person => person.id !== id);
+  res.status(204).end();
+});
+
+const generateId = () => {
+  const newId = Math.floor(Math.random() * 100000);
+  return newId;
+};
+
+// add person
+app.post('/api/persons', (req, res) => {
+  const body = req.body;
+  if (!body.name || !body.number) {
+    return res.status(400).json({
+      error: 'information incomplete'
+    });
+  } else if (persons.find(person => person.name.toLowerCase() === body.name.toLowerCase())) {
+    return res.status(400).json({
+      error: 'name already exist in phonebook'
+    });
+  } else {
+    const newPerson = {
+      name: body.name,
+      number: body.number,
+      id: generateId()
+    };
+    persons = persons.concat(newPerson);
+    res.json(newPerson);
+  }
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
