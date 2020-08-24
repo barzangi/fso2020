7722dc1441c2458a159851a078369d4049759baa
Layth Barzangi <thebarzangi@gmail.com>
2020-08-24 22:47:06 +0400

    Solved: Exercises 3.15-3.18 (Phonebook app backend) - Implemented deleting phonebook entries - Implemented modifying phonebook entries - Moved error handling to error handler middleware

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -38,17 +38,25 @@ app.get('/info', (req, res) => {
 });
 
 // return single person info
-app.get('/api/persons/:id', (req, res) => {
-  Person.findById(req.params.id).then(person => {
-    res.json(person);
-  });
+app.get('/api/persons/:id', (req, res, next) => {
+  Person.findById(req.params.id)
+  .then(person => {
+    if(person) {
+      res.json(person);
+    } else {
+      res.status(404).end();
+    }  
+  })
+  .catch(error => next(error));
 });
 
 // delete person
-app.delete('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id);
-  persons = persons.filter(person => person.id !== id);
-  res.status(204).end();
+app.delete('/api/persons/:id', (req, res, next) => {
+  Person.findByIdAndRemove(req.params.id)
+    .then(result => {
+      res.status(204).end();
+    })
+    .catch(error => next(error));
 });
 
 // add person
@@ -69,6 +77,38 @@ app.post('/api/persons', (req, res) => {
     res.json(savedPerson);
   });
 });
+
+// update person
+app.put('/api/persons/:id', (req, res, next) => {
+  const body = req.body;
+
+  const person = {
+    name: body.name,
+    number: body.number
+  };
+
+  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+    .then(updatedPerson => {
+      res.json(updatedPerson);
+    })
+    .catch(error => next(error));
+});
+
+const unknownEndpoint = (req, res) => {
+  res.status(400).send({ error: 'Unknown endpoint' });
+}
+app.use(unknownEndpoint);
+
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message);
+
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id'} );
+  }
+
+  next(error);
+}
+app.use(errorHandler);
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
