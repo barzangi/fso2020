6014572d8fa572fa8ee90a1d9b40957a4a34db03
Layth Barzangi <thebarzangi@gmail.com>
2020-11-04 23:01:04 +0400

    Solved: Exercises 8.8-8.9 (Booklibrary frontend GraphQL) - Implemented Authors view (name, birth year, number of books) - Implemented Books view (title, author, publishing year)

---
+++ part8/booklibrary-gql-frontend/src/components/Authors.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { useQuery } from '@apollo/client';
+
+import { ALL_AUTHORS } from '../queries';
+
+const Authors = (props) => {
+  const result = useQuery(ALL_AUTHORS);
+
+  if (!props.show) {
+    return null;
+  }
+
+  if (result.loading) {
+    return <div>Loading...</div>
+  }
+
+  const authors = result.data.allAuthors;
+
+  return (
+    <div>
+      <h2>Authors</h2>
+      <table>
+        <tbody>
+          <tr>
+            <th align='left'>
+              Name
+            </th>
+            <th align='left'>
+              Born
+            </th>
+            <th align='left'>
+              Books
+            </th>
+          </tr>
+          {authors.map(a =>
+            <tr key={a.name}>
+              <td>{a.name}</td>
+              <td>{a.born}</td>
+              <td>{a.bookCount}</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+
+    </div>
+  );
+};
+
+export default Authors;
---
+++ part8/booklibrary-gql-frontend/src/components/Books.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { useQuery } from '@apollo/client';
+
+import { ALL_BOOKS } from '../queries';
+
+const Books = (props) => {
+  const result = useQuery(ALL_BOOKS);
+
+  if (!props.show) {
+    return null;
+  }
+
+  if (result.loading) {
+    return <div>Loading...</div>
+  }
+
+  const books = result.data.allBooks;
+
+  return (
+    <div>
+      <h2>Books</h2>
+
+      <table>
+        <tbody>
+          <tr>
+            <th align='left'>
+              Title
+            </th>
+            <th align='left'>
+              Author
+            </th>
+            <th align='left'>
+              Published
+            </th>
+          </tr>
+          {books.map(a =>
+            <tr key={a.title}>
+              <td>{a.title}</td>
+              <td>{a.author}</td>
+              <td>{a.published}</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Books;
